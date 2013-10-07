855bf7ebe90e84ba1059bede56028edc2a9ae638
pyro2927 <joseph@pintozzi.com>
2013-10-06 21:39:08 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sshman::Application.config.secret_key_base = 'f615539876c119e807aed496ee9b5d0ae3896210cfcb9d949cce6749f9cb2bd1665637868e387ed4bcd0c80b931343ea69f2418f54de98b8837803b3ade4204d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sshman::Application.config.session_store :cookie_store, key: '_sshman_session'
